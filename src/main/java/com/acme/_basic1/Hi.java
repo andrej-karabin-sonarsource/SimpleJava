6d7073c5d140cd955c6e259c717d67a33c5fff9d
rru <ankahikahani@gmail.com>
2021-09-21 17:04:45 +0200

    Initailizing a repo

---
+++ src/main/java/com/acme/_basic1/Hi.java
@@ -0,0 +1,14 @@
+package com.acme._basic1;
+
+class Hi {
+
+  void sayHello() {
+    System.out.println("Hello World!!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests.");
+	System.out.println("new in newbranch");
+  }
+
+}
---
+++ src/main/java/com/acme/basic1.simple/Hi.java
@@ -0,0 +1,13 @@
+package com.acme.basic1.simple;
+
+class Hi {
+
+  void sayHello() {
+    System.out.println("Hello World!!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests.");
+  }
+
+}
